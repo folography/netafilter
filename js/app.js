ffe2a0dfb60476b7ba8d64109e921938048b7eec
Shreyas Satish <shreyas.satish@gmail.com>
2016-01-27 02:59:17 +0530

    working filters for education with legend

--- js/app.js
+++ js/app.js
@@ -1,147 +1,101 @@
 window.NetaFilter = {};
 
 window.NetaFilter.filterModel = [{
-  label: 'Change View',
-  layer: 'myneta-loksabha fill-0',
-  mapFilter: [],
-  filters: [{ 
     label: 'Education',
+    color: '#f2f2f2',
     fieldName: 'myneta Education Points',
-    mapFilter: [],
-    filters:[{ color: '#ff0000',
+    layer: 'education',
+    mapFilter: [["<=", "myneta Education Points", 25]],
+    selected: true,
+    filters:[{ color: 'rgb(250, 0, 0)',
         label: 'Illiterate',
-        layer: 'myneta-loksabha fill-1',
-        mapFilter: [["==", "myneta Education Points", 0]]
+        layer: 'illiterate',
+        mapFilter: [["==", "myneta Education Points", 0]],
+        selected: true
       },
-      { color: '#ff641c',
+      { color: 'rgb(250, 100, 28)',
         label: '8th pass',
-        layer: 'myneta-loksabha fill-2',
-        mapFilter: [["<=", "myneta Education Points", 9], [">", "myneta Education Points", 0]]
+        layer: '8th-pass',
+        mapFilter: [["<=", "myneta Education Points", 9], [">", "myneta Education Points", 0]],
+        selected: true
       },
-      { color: '#ffae00',
+      { color: 'rgb(255, 174, 0)',
         label: '10th pass',
-        layer: 'myneta-loksabha fill-3',
-        mapFilter: [["==", "myneta Education Points", 10]]
+        layer: '10th-pass',
+        mapFilter: [[">=", "myneta Education Points", 10], ["<=", "myneta Education Points", 12]],
+        selected: true
       },
-      { color: '#ff0',
+      { color: 'rgb(255, 255, 0)',
         label: 'Graduate',
-        layer: 'myneta-loksabha fill-4',
-        mapFilter: [["!=", "myneta Education Points", 13], ["!=", "myneta Education Points", 15]]
+        layer: 'graduate',
+        mapFilter: [[">=", "myneta Education Points", 13], ["<=", "myneta Education Points", 15]],
+        selected: true
       },
-      { color: '#eeff01',
+      { color: 'rgb(250, 255, 0)',
         label: 'Graduate Professional',
-        layer: 'myneta-loksabha fill-5',
-        mapFilter: [["!=", "myneta Education Points", 16], ["!=", "myneta Education Points", 17]]
+        layer: 'graduate-professional',
+        mapFilter: [[">=", "myneta Education Points", 16], ["<=", "myneta Education Points", 17]],
+        selected: true
       },
-      { color: '#b3ff00',
+      { color: 'rgb(179, 255, 0)',
         label: 'Post Graduate',
-        layer: 'myneta-loksabha fill-6',
-        mapFilter: [["==", "myneta Education Points", 20]]
+        layer: 'post-graduate',
+        mapFilter: [[">=", "myneta Education Points", 18], ["<=", "myneta Education Points", 20]],
+        selected: true
       },
-      { color: '#3ce83c',
+      { color: 'rgb(60, 232, 60)',
         label: 'PhD',
-        layer: 'myneta-loksabha fill-7',
-        mapFilter: [["==", "myneta Education Points", 25]]
+        layer: 'phd',
+        mapFilter: [[">", "myneta Education Points", 20], ["<=", "myneta Education Points", 25]],
+        selected: true
       }
     ]
   },
-  { 
-    label: 'Assets',
-    fieldName: 'myneta Total Assets',
-    mapFilter: [],
-    filters:[
-      { color: '#ff0000',
-        label: '< 1 Crore',
-        layer: 'myneta-loksabha fill-1',
-        mapFilter: [["<", "myneta Total Assets", 10000000]]
-      },
-      { color: '#ff641c',
-        label: '8th pass',
-        layer: 'myneta-loksabha fill-2',
-        mapFilter: [["<=", "myneta Education Points", 9], [">", "myneta Education Points", 0]]
-      },
-      { color: '#ffae00',
-        label: '10th pass',
-        layer: 'myneta-loksabha fill-3',
-        mapFilter: [["==", "myneta Education Points", 10]]
-      },
-      { color: '#ff0',
-        label: 'Graduate',
-        layer: 'myneta-loksabha fill-4',
-        mapFilter: [["!=", "myneta Education Points", 13], ["!=", "myneta Education Points", 15]]
-      },
-      { color: '#eeff01',
-        label: 'Graduate Professional',
-        layer: 'myneta-loksabha fill-5',
-        mapFilter: [["!=", "myneta Education Points", 16], ["!=", "myneta Education Points", 17]]
-      },
-      { color: '#b3ff00',
-        label: 'Post Graduate',
-        layer: 'myneta-loksabha fill-6',
-        mapFilter: [["==", "myneta Education Points", 20]]
-      },
-      { color: '#3ce83c',
-        label: 'PhD',
-        layer: 'myneta-loksabha fill-7',
-        mapFilter: [["==", "myneta Education Points", 25]]
-      }
-    ]
-  }]
-}];
+  // { 
+  //   label: 'Assets',
+  //   fieldName: 'myneta Total Assets',
+  //   layer: 'assets',
+  //   mapFilter: [["<=", "myneta Total Assets", 10000000000]],
+  //   filters:[
+  //     { color: '#ff0000',
+  //       label: '< 1 Crore',
+  //       layer: 'lt-1cr',
+  //       mapFilter: [["<=", "myneta Total Assets", 10000000]]
+  //     },
+  //     { color: '#ff641c',
+  //       label: '< 3 Crore',
+  //       layer: 'lt-3cr',
+  //       mapFilter: [[">", "myneta Total Assets", 30000000], ["<=", "myneta Total Assets", 30000000]]
+  //     }
+  //   ]
+  // }
+];
 
-// var netaFilterUpdater = function(state, action) {
-//   if (!state) {
-//     state = netaFilterModel;
-//   }
-//   switch (action.type) {
-//     case 'CHECK_FILTER':
-//       return state.map(function(node){
-//         if (node.label === action.label) {
-//           return node.active = true;
-//         }
-//       });
-//     case 'UNCHECK_FILTER':
-//       return state.map(function(node){
-//         if (node.label === action.label) {
-//           return node.active = false;
-//         }
-//       });
-//     default:
-//       return state;
-//   }
-// };
 
 window.NetaFilter.filterView = {
   init: function(filters){
+    var self = this;
     this.ractive = new Ractive({
       el: '#netafilters',
       template: '#netafilters-template',
       data: {
-        filters: filters,
-        selectedFilters: []
+        filters: filters
       }
     });
-    this.ractive.on('filterChange', function(e, layer, mapFilter){
-      var self = this;
-      // var checked = $.map($('form').find('input:checkbox:checked'), function(el){return $(el).data('label')});
-      console.log(mapFilter);
-      if (e.node.checked){
-        this.set('selectedFilters', [].concat(this.get('selectedFilters'), {layer: layer, mapFilter: mapFilter}));
-      } else {
-        this.set('selectedFilters', this.get('selectedFilters').filter(function(f){f !== label;}));
-      }                 
+    this.ractive.on('filterChange', function(e, layer, mapFilter, selected){
+      window.NetaFilter.mapView.toggleVisibility(layer);
     })
   }
 };
 
 window.NetaFilter.mapView = {
-  init: function(config){
+  init: function(filters){
     var self = this;
     
     mapboxgl.accessToken = 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ';
     self.map = new mapboxgl.Map({
       container: 'map', // container id
-      style: 'mapbox://styles/planemad/ciix3a6g900tkalkidus93oy3', //stylesheet location
+      style: 'mapbox://styles/planemad/cijswl6y7009rcakwxo2nuu7x',
       hash: true
     });
 
@@ -150,70 +104,124 @@ window.NetaFilter.mapView = {
         selectedConstituency: ''
       },
       oninit: function(){
-        self.map.off('tile.error', self.map.onError);
-        self.map.on('click', function(e){
-          self.map.featuresAt(e.point, {
-            layer: [config.layer],
-            radius: 4,
-            includeGeometry: true
-          }, function(err, features) {
-            // // Reset the tooltip if a different constituency is selected
-            // if (this.get('selectedConstituency') !== features[0].properties['PC_NAME2']) {
-            //   tooltip.setFeatures(features);
-            //   this.set('selectedConstituency', features[0].properties['PC_NAME2']);
-            // } else {
-            //   // Selected constituency was unselected.
-            //   this.set('selectedConstituency', '');
-            // }
-            // this.get('map').setFilter('myneta-loksabha selected', ['==', 'PC_NAME2', this.get('selectedConstituency')]);
-          });
-        });
 
-        self.map.on('mousemove', function(e) {
-          self.map.featuresAt(e.point, {
-            layer: [config.baseLayer],
-            radius: 4
-          }, function(err, features) {
-            // Reset tooltip only if no constituency is currently selected.
-            // if (this.get('selectedConstituency') === '') {
-            //   tooltip.setFeatures(features);
-            // }
+        self.map.off('tile.error', self.map.onError);
+
+        var treeToLayer = function(tree){
+          var parentLayer;
+          tree.forEach(function(node){
+            parentLayer = node.layer;
+            self.map.on('style.load', function () {
+              self.createLayer({name: node.layer, mapFilter: node.mapFilter, color: node.color, selected: node.selected});
+              if (typeof node.filters === 'object') {
+                node.filters.forEach(function(subnode){
+                  self.createLayer({name: subnode.layer, mapFilter: subnode.mapFilter, color: subnode.color, selected: subnode.selected});
+                })
+              }
+            });
           });
+        }
+
+        treeToLayer(filters);
+
+        self.map.on('style.load', function () {
+          var tooltip = new Ractive({
+            el: '#map-tooltip',
+            template: '#myneta-template',
+            data: {},
+            setFeatures: function(features){
+              this.set({
+                id: features[0].properties['myneta Sno'],
+                candidate: features[0].properties['myneta Candidate'],
+                constituency: features[0].properties['PC_NAME2'],
+                state: features[0].properties['ST_NAME'],
+                category: features[0].properties['Res'],
+                party: features[0].properties['myneta Party'],
+                cases: features[0].properties['myneta Criminal Case'],
+                qualification: features[0].properties['myneta Education'],
+                assets: (features[0].properties['myneta Total Assets']/10000000).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' crore',
+                liabilities: (features[0].properties['myneta Liabilities']/10000000).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' crore'
+              });
+            }
+          });
+          
+          var selectedConstituency = '';
+          var selectedLayer = self.map.addLayer({
+           "id": "selected-constituency",
+           "type": "line",
+           "source": "mapbox://planemad.6wpgu5qz",
+           "source-layer": "myneta-loksabha",
+           "layout": {
+              "visibility": "none"
+           },
+           "paint": {
+              "line-color": 'cyan',
+              "line-opacity": 1.0,
+              "line-width": 5
+           },
+           "hidden": false
+          }, 'aeroway_runway');
+
+          self.map.on('click', function(e){
+            self.map.featuresAt(e.point, {
+              layer: ['myneta-baselayer'],
+              radius: 4,
+              includeGeometry: true
+            }, function(err, features) {
+              // Reset the tooltip if a different constituency is selected
+              console.log(features);
+              if (selectedConstituency !== features[0].properties['PC_NAME2']) {
+                tooltip.setFeatures(features);
+                selectedConstituency = features[0].properties['PC_NAME2'];
+                self.map.setFilter('selected-constituency', ['==', 'PC_NAME2', selectedConstituency]);
+                self.map.setLayoutProperty('selected-constituency', 'visibility', 'visible');
+              } else {
+                // Selected constituency was unselected.
+                selectedConstituency = '';
+                self.map.setLayoutProperty('selected-constituency', 'visibility', 'none');
+              }
+            });
+          });
+
+          self.map.on('mousemove', function(e) {
+            self.map.featuresAt(e.point, {
+              layer: ['myneta-baselayer'],
+              radius: 4
+            }, function(err, features) {
+              // Reset tooltip only if no constituency is currently selected.
+              if (selectedConstituency === '' && features.length > 0) {
+                tooltip.setFeatures(features);
+              }
+            });
+          });
+
         });
       }
     });
   },
-  setFilter: function(layer, filter){
-    this.map.setFilter(layer, filter);
+  createLayer: function(config){
+    this.map.addLayer({
+     "id": config.name,
+     "type": "fill",
+     "source": "mapbox://planemad.6wpgu5qz",
+     "source-layer": "myneta-loksabha",
+     "filter": [].concat('all', config.mapFilter),
+     "layout": {
+        "visibility": config.selected ? "visible" : "none"
+     },
+     "paint": {
+        "fill-color": config.color,
+        "fill-opacity": 1.0,
+        "visibility": "visible"
+     },
+     "hidden": false
+    }, 'aeroway_runway');
   },
-  applyFilters: function(selectedFilters){
-    var self = this;
-    selectedFilters.forEach(function(selectedFilter){
-      selectedFilter.mapFilter.forEach(function(filter){
-        self.setFilter(selectedFilter.layer, filter);
-      })
-    });
-    // filters.forEach(function(node){
-    //   console.log(selectedFilters);
-    //   console.log(node.label);
-    //   if (selectedFilters.indexOf(node.label) > -1) {
-    //     node.filter.forEach(function(mapFilter){
-    //       console.log(node.layer);
-    //       console.log(mapFilter);
-    //       netaMapView.setFilter(node.layer, mapFilter); 
-    //     });
-    //   }
-    // })
+  toggleVisibility: function(layer){
+    var status = this.map.getLayoutProperty(layer, 'visibility');
+    this.map.setLayoutProperty(layer, 'visibility', status === 'visible' ? 'none' : 'visible');
   }
 };
 
-// var netaFilterUpdate = Redux.createStore(netaFilterUpdater);
-// netaFilterStore.subscribe(function(){
-//   netaFilterView.set('todos', todoStore.getState());
-// })
-// netaFilterView.init(netaFilterUpdate.getState());
 window.NetaFilter.filterView.init(window.NetaFilter.filterModel);
-window.NetaFilter.mapView.init(window.NetaFilter.filterModel[0].layer);
-// netaFilterView.ractive.on('filter-changed', function(){
-  // netaMapView.setFilter(netaFilterUpdate.dispatch({type: , label: }));
-// })
+window.NetaFilter.mapView.init(window.NetaFilter.filterModel);
