4ab249645e888f526f7cfb7a6d92eff504b6ab64
Arun Ganesh <arun.planemad@gmail.com>
2016-08-15 16:48:32 +0530

    Sanitize party name classes

--- js/map.js
+++ js/map.js
@@ -1,2 +1,22 @@
-// Simple map
-mapboxgl.accessToken = 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ';
+// Dependencies
+var Config = require('./config');
+mapboxgl.accessToken = Config.accessToken;
+
+var Merge = require('merge');
+
+// Configure map creation options
+// https://www.mapbox.com/mapbox-gl-js/api/#Map
+var mapOptions = Merge({
+    hash: true
+}, Config.map);
+
+var map = new mapboxgl.Map(mapOptions);
+
+// Add default controls
+var geolocate = map.addControl(new mapboxgl.Geolocate({
+    position: 'bottom-right'
+}));
+map.addControl(new mapboxgl.Navigation());
+
+// Export module
+module.exports = map;
